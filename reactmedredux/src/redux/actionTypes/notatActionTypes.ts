acec1edaa4d42552d465d83e499ff6eddaf0f28d
Asgeir Hunshamar admin <admahuns@utvikling.local>
2022-03-11 17:16:23 +0100

    git bug

---
+++ reactmedredux/src/redux/actionTypes/notatActionTypes.ts
@@ -0,0 +1,9 @@
+enum notatActionTypes {
+  REQUEST_START = "REQUEST_START",
+  REQUEST_FAILED = "REQUEST_FAILED",
+  HENT_NOTATER = "HENT_NOTATER",
+  LEGG_TIL_NOTAT = "LEGG_TIL_NOTAT",
+  SLETT_NOTAT = "SLETT_NOTAT",
+}
+
+export default notatActionTypes;
